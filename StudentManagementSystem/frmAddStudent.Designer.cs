54f282dbdbdd9ae4bece30e1fb958799ef43e79b
Hassan Nasar <Hassan Nasar@DESKTOP-8TQ450O>
2022-09-28 21:42:44 +0500

    student commit

---
+++ StudentManagementSystem/frmAddStudent.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace StudentManagementSystem
+{
+    partial class frmAddStudent
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSave = new System.Windows.Forms.Button();
+            this.cboClass = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtFName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtFCNIC = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtCell = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtSection = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtAdmDate = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(387, 33);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 0;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // cboClass
+            // 
+            this.cboClass.FormattingEnabled = true;
+            this.cboClass.Location = new System.Drawing.Point(162, 254);
+            this.cboClass.Name = "cboClass";
+            this.cboClass.Size = new System.Drawing.Size(160, 21);
+            this.cboClass.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "ID";
+            // 
+            // txtID
+            // 
+            this.txtID.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtID.Location = new System.Drawing.Point(162, 30);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(160, 26);
+            this.txtID.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Student Name";
+            // 
+            // txtSName
+            // 
+            this.txtSName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSName.Location = new System.Drawing.Point(162, 67);
+            this.txtSName.Name = "txtSName";
+            this.txtSName.Size = new System.Drawing.Size(160, 26);
+            this.txtSName.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 99);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Father Name";
+            // 
+            // txtFName
+            // 
+            this.txtFName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFName.Location = new System.Drawing.Point(162, 99);
+            this.txtFName.Name = "txtFName";
+            this.txtFName.Size = new System.Drawing.Size(160, 26);
+            this.txtFName.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 142);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Father CNIC";
+            // 
+            // txtFCNIC
+            // 
+            this.txtFCNIC.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFCNIC.Location = new System.Drawing.Point(162, 134);
+            this.txtFCNIC.Name = "txtFCNIC";
+            this.txtFCNIC.Size = new System.Drawing.Size(160, 26);
+            this.txtFCNIC.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 174);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(52, 13);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Address";
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAddress.Location = new System.Drawing.Point(162, 166);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(160, 26);
+            this.txtAddress.TabIndex = 3;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(12, 215);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(28, 13);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Cell";
+            // 
+            // txtCell
+            // 
+            this.txtCell.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCell.Location = new System.Drawing.Point(162, 207);
+            this.txtCell.Name = "txtCell";
+            this.txtCell.Size = new System.Drawing.Size(160, 26);
+            this.txtCell.TabIndex = 3;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(12, 254);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(37, 13);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "Class";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(12, 289);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(50, 13);
+            this.label8.TabIndex = 2;
+            this.label8.Text = "Section";
+            // 
+            // txtSection
+            // 
+            this.txtSection.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSection.Location = new System.Drawing.Point(162, 289);
+            this.txtSection.Name = "txtSection";
+            this.txtSection.Size = new System.Drawing.Size(160, 26);
+            this.txtSection.TabIndex = 3;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(12, 333);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(94, 13);
+            this.label9.TabIndex = 2;
+            this.label9.Text = "Admission Date";
+            // 
+            // txtAdmDate
+            // 
+            this.txtAdmDate.Location = new System.Drawing.Point(162, 333);
+            this.txtAdmDate.Name = "txtAdmDate";
+            this.txtAdmDate.Size = new System.Drawing.Size(160, 20);
+            this.txtAdmDate.TabIndex = 4;
+            // 
+            // frmAddStudent
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(541, 450);
+            this.Controls.Add(this.txtAdmDate);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.txtSection);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtCell);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtFCNIC);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtFName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtSName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cboClass);
+            this.Controls.Add(this.btnSave);
+            this.Name = "frmAddStudent";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.ComboBox cboClass;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtFName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtFCNIC;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtCell;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtSection;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.DateTimePicker txtAdmDate;
+    }
+}
+
---
+++ StudentManagementSystem/frmAddStudent.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudentManagementSystem
+{
+    public partial class frmAddStudent : Form
+    {
+        public frmAddStudent()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection("Data Source=.;Initial Catalog=SDB;Integrated Security=True");
+                SqlCommand cmd = new SqlCommand("AddStudent", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add("@SID", SqlDbType.NVarChar, 50).Value = txtID.Text;
+                cmd.Parameters.Add("@StudentName", SqlDbType.NVarChar, 50).Value = txtSName.Text;
+                cmd.Parameters.Add("@FatherName", SqlDbType.NVarChar, 50).Value = txtFName.Text;
+                cmd.Parameters.Add("@FatherCNIC", SqlDbType.NVarChar, 50).Value = txtFCNIC.Text;
+                cmd.Parameters.Add("@Address", SqlDbType.NVarChar, 50).Value = txtAddress.Text;
+                cmd.Parameters.Add("@Cell", SqlDbType.NVarChar, 50).Value = txtCell.Text;
+                cmd.Parameters.Add("@Class", SqlDbType.NVarChar, 50).Value = cboClass.Text;
+                cmd.Parameters.Add("@Section", SqlDbType.NVarChar, 50).Value = txtSection.Text;
+                cmd.Parameters.Add("@AdmDate", SqlDbType.NVarChar, 50).Value = txtAdmDate.Text;
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+            }
+           catch(Exception ex)
+            {
+                MessageBox.Show("Error:" + ex.Message);
+            }
+
+        }
+    }
+}
